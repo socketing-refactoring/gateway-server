d7545d13d6745d508dd47f139fce2a8c90fd6efc
jee-in <chris309804@gmail.com>
2025-03-23 16:21:53 +0900

    fix: Resolve redundant cors headers error

--- src/main/java/com/jeein/gateway/GlobalCorsConfiguration.java
+++ src/main/java/com/jeein/gateway/GlobalCorsConfiguration.java
@@ -1,6 +1,7 @@
 package com.jeein.gateway;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,19 +9,25 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@Slf4j
 public class GlobalCorsConfiguration {
 
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOriginPatterns(List.of("*"));
-        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfig.setAllowedMethods(List.of("*"));
         corsConfig.setAllowedHeaders(List.of("*"));
         corsConfig.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
 
+        log.info("Allowed Origins: {}", corsConfig.getAllowedOrigins());
+        log.info("Allowed Origin Patterns: {}", corsConfig.getAllowedOriginPatterns());
+        log.info("Allowed Methods: {}", corsConfig.getAllowedMethods());
+        log.info("Allowed Headers: {}", corsConfig.getAllowedHeaders());
+        log.info("Allow Credentials: {}", corsConfig.getAllowCredentials());
         return new CorsWebFilter(source);
     }
 }
--- src/main/java/com/jeein/gateway/filter/LoggingGlobalFilter.java
+++ src/main/java/com/jeein/gateway/filter/LoggingGlobalFilter.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Slf4j
 @Component
 @Order(0)
@@ -41,6 +43,56 @@ public class LoggingGlobalFilter implements GlobalFilter {
                                     log.info(
                                             "Response status code: {}",
                                             exchange.getResponse().getStatusCode());
+
+                                    // 변경 전 헤더 로그
+                                    var headers = exchange.getResponse().getHeaders();
+
+                                    log.info("Before header modification:");
+                                    headers.forEach((key, value) -> log.info("{}: {}", key, value));
+
+
+                                    // 중복 제거 (Origin, Credentials)
+                                    String allowHeaders = headers.getFirst("Access-Control-Allow-Headers");
+                                    if (allowHeaders != null) {
+                                        String cleanedAllowHeaders = allowHeaders.replaceAll("[\\[\\]]", "");
+                                        String[] allowHeadersArray = cleanedAllowHeaders.split(",\\s*");
+
+                                        headers.put("Access-Control-Allow-Headers", List.of(allowHeadersArray[0]));
+                                    }
+
+                                    String allowCredentials = headers.getFirst("Access-Control-Allow-Credentials");
+                                    if (allowCredentials != null) {
+                                        String cleanedCredentialsHeaders = allowCredentials.replaceAll("[\\[\\]]", "");
+                                        String[] allowCredentialsArray = cleanedCredentialsHeaders.split(",\\s*");
+
+                                        headers.put("Access-Control-Allow-Credentials", List.of(allowCredentialsArray[0]));
+                                    }
+
+                                    String allowOrigins = headers.getFirst("Access-Control-Allow-Origin");
+                                    if (allowOrigins != null) {
+                                        String cleanedAllowOrigins = allowOrigins.replaceAll("[\\[\\]]", "");
+                                        String[] allowOriginsArray = cleanedAllowOrigins.split(",\\s*");
+
+                                        headers.put("Access-Control-Allow-Origin", List.of(allowOriginsArray[0]));
+                                    }
+
+                                    // "Vary" 헤더 제거
+                                    boolean removed = exchange.getResponse().getHeaders().keySet().removeIf(headerName -> headerName.equalsIgnoreCase("Vary"));
+
+                                    if (removed) {
+                                        log.info("Vary header was removed successfully.");
+                                    } else {
+                                        log.info("No Vary header found.");
+                                    }
+
+                                    // Vary 헤더 추가
+                                    exchange.getResponse().getHeaders().add("Vary", "Origin");
+                                    exchange.getResponse().getHeaders().add("Vary", "Access-Control-Request-Method");
+                                    exchange.getResponse().getHeaders().add("Vary", "Access-Control-Request-Headers");
+
+                                    // 변경 후 헤더 로그
+                                    log.info("After header modification:");
+                                    exchange.getResponse().getHeaders().forEach((key, value) -> log.info("{}: {}", key, value));
 
                                     Route route =
                                             exchange.getAttribute(
