07541b9ce65ee0fa230fc242afa7ac84fb718a9d
jee-in <chris309804@gmail.com>
2025-04-25 20:25:57 +0900

    fix: Fix exchange response commit error

--- src/main/java/com/jeein/gateway/filter/LoggingGlobalFilter.java
+++ src/main/java/com/jeein/gateway/filter/LoggingGlobalFilter.java
@@ -1,6 +1,5 @@
 package com.jeein.gateway.filter;
 
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -23,7 +22,6 @@ public class LoggingGlobalFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        // Pre Filter Logic
         ServerHttpRequest request = exchange.getRequest();
 
         String uri = request.getURI().toString();
@@ -38,84 +36,62 @@ public class LoggingGlobalFilter implements GlobalFilter {
                 .then(
                         Mono.fromRunnable(
                                 () -> {
-                                    // Post Filter Logic
-                                    log.info(
-                                            "Response status code: {}",
-                                            exchange.getResponse().getStatusCode());
-
-                                    // 변경 전 헤더 로그
-                                    var headers = exchange.getResponse().getHeaders();
-
-                                    log.debug("Before header modification:");
-                                    headers.forEach(
-                                            (key, value) -> log.debug("{}: {}", key, value));
-
-                                    // 중복 제거 (Origin, Credentials)
-                                    String allowHeaders =
-                                            headers.getFirst("Access-Control-Allow-Headers");
-                                    if (allowHeaders != null) {
-                                        String cleanedAllowHeaders =
-                                                allowHeaders.replaceAll("[\\[\\]]", "");
-                                        String[] allowHeadersArray =
-                                                cleanedAllowHeaders.split(",\\s*");
-
-                                        headers.put(
-                                                "Access-Control-Allow-Headers",
-                                                List.of(allowHeadersArray[0]));
-                                    }
-
-                                    String allowCredentials =
-                                            headers.getFirst("Access-Control-Allow-Credentials");
-                                    if (allowCredentials != null) {
-                                        String cleanedCredentialsHeaders =
-                                                allowCredentials.replaceAll("[\\[\\]]", "");
-                                        String[] allowCredentialsArray =
-                                                cleanedCredentialsHeaders.split(",\\s*");
-
-                                        headers.put(
-                                                "Access-Control-Allow-Credentials",
-                                                List.of(allowCredentialsArray[0]));
-                                    }
-
-                                    String allowOrigins =
-                                            headers.getFirst("Access-Control-Allow-Origin");
-                                    if (allowOrigins != null) {
-                                        String cleanedAllowOrigins =
-                                                allowOrigins.replaceAll("[\\[\\]]", "");
-                                        String[] allowOriginsArray =
-                                                cleanedAllowOrigins.split(",\\s*");
-
-                                        headers.put(
-                                                "Access-Control-Allow-Origin",
-                                                List.of(allowOriginsArray[0]));
-                                    }
-
-                                    // "Vary" 헤더 제거
-                                    boolean removed =
-                                            exchange.getResponse()
-                                                    .getHeaders()
-                                                    .keySet()
-                                                    .removeIf(
-                                                            headerName ->
-                                                                    headerName.equalsIgnoreCase(
-                                                                            "Vary"));
-
-                                    if (removed) {
-                                        log.debug("Vary header was removed successfully.");
-                                    } else {
-                                        log.debug("No Vary header found.");
-                                    }
-
-                                    // Vary 헤더 추가
-                                    exchange.getResponse().getHeaders().add("Vary", "Origin");
                                     exchange.getResponse()
-                                            .getHeaders()
-                                            .add("Vary", "Access-Control-Request-Method");
-                                    exchange.getResponse()
-                                            .getHeaders()
-                                            .add("Vary", "Access-Control-Request-Headers");
+                                            .beforeCommit(
+                                                    () -> {
+                                                        var headers =
+                                                                exchange.getResponse().getHeaders();
 
-                                    // 변경 후 헤더 로그
+                                                        log.debug("Before header modification:");
+                                                        headers.forEach(
+                                                                (key, value) ->
+                                                                        log.debug(
+                                                                                "{}: {}", key,
+                                                                                value));
+
+                                                        String allowHeaders =
+                                                                headers.getFirst(
+                                                                        "Access-Control-Allow-Headers");
+                                                        if (allowHeaders != null) {
+                                                            headers.set(
+                                                                    "Access-Control-Allow-Headers",
+                                                                    allowHeaders.split(",")[0]
+                                                                            .trim());
+                                                        }
+
+                                                        String allowCredentials =
+                                                                headers.getFirst(
+                                                                        "Access-Control-Allow-Credentials");
+                                                        if (allowCredentials != null) {
+                                                            headers.set(
+                                                                    "Access-Control-Allow-Credentials",
+                                                                    allowCredentials.split(",")[0]
+                                                                            .trim());
+                                                        }
+
+                                                        String allowOrigins =
+                                                                headers.getFirst(
+                                                                        "Access-Control-Allow-Origin");
+                                                        if (allowOrigins != null) {
+                                                            headers.set(
+                                                                    "Access-Control-Allow-Origin",
+                                                                    allowOrigins.split(",")[0]
+                                                                            .trim());
+                                                        }
+
+                                                        headers.remove("Vary");
+                                                        headers.add("Vary", "Origin");
+                                                        headers.add(
+                                                                "Vary",
+                                                                "Access-Control-Request-Method");
+                                                        headers.add(
+                                                                "Vary",
+                                                                "Access-Control-Request-Headers");
+
+                                                        return Mono.empty();
+                                                    });
+
+                                    // Post-commit 로그 출력
                                     log.info("After header modification:");
                                     exchange.getResponse()
                                             .getHeaders()
